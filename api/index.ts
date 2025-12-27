a7431211ee8e05c742ca14aa1575adc54adcdbd0
PradeepAbburi <dineshkrishnapradeep@gmail.com>
2025-12-27 17:47:36 +0530

    Configure project for Vercel deployment: add api/index.ts, vercel.json, and refactor server setup

---
+++ api/index.ts
@@ -0,0 +1,11 @@
+import { setupApp } from "../server/app_factory";
+
+let appPromise: Promise<any> | null = null;
+
+export default async function handler(req: any, res: any) {
+    if (!appPromise) {
+        appPromise = setupApp().then(({ app }) => app);
+    }
+    const app = await appPromise;
+    app(req, res);
+}
